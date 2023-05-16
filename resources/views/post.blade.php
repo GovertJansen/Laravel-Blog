0603a6b699db2139b66d0df1f68b317c9ccec88f
Govert Jansen <97050782@st.deltion.nl>
2023-05-16 11:49:47 +0200

    My first commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="welcome.css">
+</head>
+
+<body>
+    <div class="bvg">
+        <h1>Blog Van Govert</h1>
+        <h2>Blog Van Govert</h2>
+        <h3>Blog Van Govert</h3>
+        <h4>Blog Van Govert</h4>
+        <h5>Blog Van Govert</h5>
+        <H6>Blog Van Govert</H6>
+    </div>
+
+    <article class="mfp">
+        <h1><a href="/post"> My first post</a> </h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nisi risus, congue eget elit in, malesuada convallis purus. Ut ipsum ligula, aliquam at gravida at, finibus sed odio. Quisque porttitor euismod metus, sollicitudin commodo justo fringilla sed. Curabitur facilisis condimentum erat nec lobortis. Phasellus ultrices sapien leo, a porta metus hendrerit quis. Nunc elit felis, posuere sit amet leo quis, faucibus sagittis ante. Morbi ultricies id odio eget ornare. Nam sapien neque, mollis et egestas et, lacinia eget elit. Integer ut lorem at tellus facilisis condimentum quis et augue. Ut lorem dolor, bibendum in felis id, mollis feugiat massa. Quisque eu mattis leo. Vivamus metus orci, pulvinar non eros eget, bibendum aliquet quam. Vestibulum placerat dignissim augue sed condimentu Pellentesque eget odio ut sem efficitur tempus. In leo massa, viverra ut venenatis sit amet, tempus vel velit. Donec sollicitudin odio nec augue maximus pulvinar. Pellentesque felis ipsum, interdum id leo non, sodales suscipit libero. Nunc placerat lorem consequat turpis convallis, sed feugiat odio mollis. Morbi convallis sem eget orci lobortis, id aliquet mauris consequat. Sed blandit justo nec eros dignissim molestie. Donec faucibus nulla ut molestie semper. Aliquam euismod nisi a posuere tincidunt. Nam vehicula pharetra dolor in tempor. Fusce vel vulputate justo. Fusce blandit ac eros at viverra. Phasellus vel massa odio. Integer consectetur, metus ac consequat aliquam, leo nibh euismod felis, sit amet faucibus neque nibh sed est. Donec sollicitudin id nisl et aliquet. Praesent egestas ullamcorper enim, nec molestie justo tempus nec.
+        </p>
+    </article>
+
+    <a class="goback" href="/">Go back</a>
+
+</body>
+
+</html>
